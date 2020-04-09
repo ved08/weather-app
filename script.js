2d09f324850ba35e52f1ebd8d8ea60305bdd2c82
Vedvardhan <vedvardhan.g@gmail.com>
2020-04-09 21:42:04 +0530

    Fixed bugs

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ let input = document.getElementById('inputValue');
 let output = document.getElementById('output');
 let humidity = document.getElementById('humidity');
 function showInfo(){
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=2c11cc38f5a4850231888e39ff9fd5fa&units=metric')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=2c11cc38f5a4850231888e39ff9fd5fa&units=metric')
     .then(resp => resp.json())
     .then(data => {
         output.textContent = `Temperature in ${input.value} is ${data.main.temp}°C`
         humidity.textContent = `Humidity is ${data.main.humidity}%`
     })
-    .catch(err => alert("The city you have entered does not exist in our database"))
+    .catch(err => alert("The city you have entered does not exist in the database"))
 }
